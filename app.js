6d4cda192c1061f89f450977f450db6d56a601b7
moikill231 <aaro.kivimaki@gmail.com>
2025-10-21 16:45:52 +0300

    testaus

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.json({ message: 'Hello from backend' })
 })
 
 app.listen(port, () => {
