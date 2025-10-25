758deae7a9a9dd5f695ea7ba55f9b7caaef758f6
moikill231 <aaro.kivimaki@gmail.com>
2025-10-25 19:03:29 +0300

    Saatu valmiiks? backend hakee tietokannasta tarvittavia tietoja

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mysql = require("mysql2")
 const port = 3000
 
 app.use(cors())
 
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'node_kayttaja',
+	password: '2314Juustokakku!',
+	database: 'Tuotteet'
+})
+
+connection.connect((err) => {
+	if (err) {
+		console.log("Connection to database unsuccesful", err)
+	} else {
+		console.log("Connection to database succesful")
+	}
+
+})
+
 app.get('/', (req, res) => {
-	res.json({ message: 'Hello from backend' })
+	connection.query('SELECT * FROM raaka_puut', (err, results) => {
+
+		if (err) {
+			console.log("Database query failed", err)
+		} else {
+			console.log(results)
+		}
+		res.json(results)
+	})
 })
 
 app.listen(port, () => {
