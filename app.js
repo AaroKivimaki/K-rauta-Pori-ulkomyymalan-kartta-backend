eb2b00142519191e4b518cecee8225a3ed1f39ff
Aaro <aaro.kivimaki@gmail.com>
2025-12-16 13:20:29 +0200

    aloitettu tekemään jwt authentikaatiota

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const app = express()
 const cors = require('cors')
 const mysql = require("mysql2")
 const port = 3000
-
+const jwt = require("jsonwebtoken")
+app.use(express.json())
 app.use(cors())
+require("dotenv").config()
+
+const users = [
+	{
+		id: 1,
+		username: "muokkaaja",
+		password: "muokkaaja123",
+		isAdmin: true
+	}
+]
+
+const secretKey = process.env.SECRET_KEY
 
 const connection = mysql.createConnection({
 	host: 'localhost',
@@ -20,6 +33,20 @@ connection.connect((err) => {
 		console.log("Connection to database succesful")
 	}
 
+})
+
+app.post("/login", (req, res) => {
+	const { username, password } = req.body
+
+	const user = users.find(u => {
+		return u.username === username && u.password === password
+	})
+
+	if (user) {
+		const accessToken = jwt.sign({ id: user.id, isAdmin: user.isAdmin })
+	} else {
+		res.status(400).json("Username or password incorrect")
+	}
 })
 
 app.get('/', (req, res) => {
